c9394921264b8e18cebf36537049b13bb403734d
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-09-16 15:38:16 -0400

    innerhtml to dynamically render posts Currently working on increasing likes when dbl clicking post

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const posts = [
         comment: "i'm feelin a bit stressed tbh",
         likes: 4
     },
-        {
+    {
         name: "Joseph Ducreux",
         username: "jd1735",
         location: "Paris, France",
@@ -37,95 +37,200 @@ const mainEl = document.querySelector(".container");
 
 // Create section element and add it to main El
 const createSectionEl = document.createElement("section");
-// createSectionEl.classList.add("test-border");
-mainEl.append(createSectionEl); 
 
-// Create profile 
-const profileSectionEl = document.createElement("div");
-profileSectionEl.classList.add("profile");
 
-// Create img element
-const imgProfileEl = document.createElement("img");
-imgProfileEl.classList.add("profile-img");
-imgProfileEl.setAttribute("src", posts[0].avatar);
-imgProfileEl.setAttribute("alt", `Portrait of ${posts[0].avatar}`);
+for(let i = 0; i < posts.length; i++) {
+    createSectionEl.innerHTML += `<section>
+                                <div class="profile">
+                                    <img src="${posts[i].avatar}" alt="Picture of ${posts[i].name}" class="profile-img" >
+                                    <div class="profile-info">
+                                        <p class="profile-name bold-text" id="user-name">${posts[i].name}</p>
+                                        <p class="profile-location" id="user-location"> ${posts[i].location}</p>
+                                    </div>
+                                </div>
+                                <div class="img-container" id="img-post" >
+                                    <img class="post" src="${posts[i].post}" alt="Self-portrait painting of ${posts[i].name}" id="post-${i}"> 
+                                </div>
+                                <div class="comment-section">
+                                    <div class="icon-section">
+                                        <img class="comment-icons" src ="images/icon-heart.png">
+                                        <img class="comment-icons" src ="images/icon-comment.png">
+                                        <img class="comment-icons" src ="images/icon-dm.png">
+                                    </div>
+                                    <p class="like-section bold-text" id="like-count${[i]}">${posts[i].likes} likes</p>
+                                    <p class="comment"><span class="bold-text" id="user-name">${posts[i].username}</span>${posts[i].comment}</p>
+                                </div>
+                            </section> `;
+    mainEl.append(createSectionEl); 
+}
 
-// Create div for profile info
-const profileInfoEl = document.createElement("div");
-profileSectionEl.classList.add("profile-info");
+// Create variables 
+function createIdForEvntListeners() {
+    let clickPostArr = [];
+    for(let i = 0; i < posts.length; i++) {
+        clickPostArr.push(document.getElementById("post-"+[i]));        
+    }
+    return clickPostArr;
+}
+let check = createIdForEvntListeners();
+console.log(check[0]);
+// // let check = createIdForEvntListeners(); 
+// // console.log(check[0]);
 
-// Create porfile name and location element
-const profileNameEl = document.createElement("p");
-profileNameEl.setAttribute("id", "user-name");
-profileNameEl.classList.add("profile-name");
-profileNameEl.classList.add("bold-text");
-profileNameEl.textContent = `${posts[0].name}`;
+// console.log(createIdForEvntListeners());
 
-const profileLocationEl = document.createElement("p");
-profileLocationEl.setAttribute("id", "user-location");
-profileLocationEl.classList.add("profile-location");
-profileLocationEl.textContent = `${posts[0].location}`;
+// const likeCountEl = document.getElementById("")
 
-// add profileNameEl & profileLocationEl to profileInfoEl, then add imgProfileEl & profileInfoEl to 
-// profileSectionEl then add that to that to create section.
-profileInfoEl.append(profileNameEl, profileLocationEl);
-profileSectionEl.append(imgProfileEl, profileInfoEl);
-createSectionEl.append(profileSectionEl);
 
-// create div with class for img post and create img tag
-const imgContainerEl = document.createElement("div")
-imgContainerEl.classList.add("img-container");
-imgContainerEl.setAttribute("id", "img-post");
-const imgEl = document.createElement("img");
-imgEl.setAttribute("src", posts[0].post);
-imgEl.setAttribute("alt", `Self-portrait of ${posts[0].name}`)
-imgContainerEl.append(imgEl);
-// Add imgContainer div to section
-createSectionEl.append(imgContainerEl);
+// Create event listeners to listen for dbl clicks on each img post via id. Which is selected from the created variables matching the 
+// id post number generated from the for loop.
+// Increase the like count when double clicking the posts.
+// function createEventListeners() {
+    
+//     const clickPosts = createIdForEvntListeners();
+//     clickPosts.forEach(post => {
+//         post.addEventListener('dblclick', function() {
 
-// create 2 divs: 1st child div containing the comment icons
-//                parent div containing 2 p tags w/ a span and child div
-const cmntSectionEl = document.createElement("div");
-cmntSectionEl.classList.add("comment-section");
-const iconSectionEl = document.createElement("div");
-iconSectionEl.classList.add("icon-section");
+//         })
+//     })
 
-// Create img tags for comment icons
-// Create img elements
-const iconHeartEl = document.createElement("img");
-iconHeartEl.classList.add("comment-icons")
-iconHeartEl.setAttribute("src", "images/icon-heart.png")
-iconHeartEl.setAttribute("alt", `Heart icon`);
+// }
 
-const iconCmntEl = document.createElement("img");
-iconCmntEl.classList.add("comment-icons")
-iconCmntEl.setAttribute("src", "images/icon-comment.png")
-iconCmntEl.setAttribute("alt", `Comment icon`);
 
-const iconDmEl = document.createElement("img");
-iconDmEl.classList.add("comment-icons")
-iconDmEl.setAttribute("src", "images/icon-dm.png")
-iconDmEl.setAttribute("alt", `Direct Message icon`);
 
-// Add the images to icon section div
-iconSectionEl.append(iconHeartEl, iconCmntEl, iconDmEl);
 
-// Create 2 p tags with the 2nd p container span 
-const likeSectionEl = document.createElement("p");
-likeSectionEl.classList.add("like-section");
-likeSectionEl.classList.add("bold-text");
-likeSectionEl.textContent = `${posts[0].likes} likes`;
 
-const cmntEl = document.createElement("p"); 
-cmntEl.classList.add("comment");
-const userNameEl = document.createElement("span");
-userNameEl.classList.add("bold-text")
-userNameEl.setAttribute("id", "user-name")
-userNameEl.textContent = `${posts[0].username}`;
-cmntEl.append(userNameEl);
 
-cmntSectionEl.append(iconSectionEl, likeSectionEl, cmntEl);
-createSectionEl.append(cmntSectionEl);
+
+
+// let count = 0;
+// const imgContainerEl = document.getElementById("img-post");
+// const likeIconEl = document.querySelector(".like-icon");
+// const likeEl = document.querySelector(".like");
+
+// imgContainerEl.addEventListener("dblclick", function() {
+//     count++;
+//     likeIconEl.classList.add("like");
+//     setTimout (() => {
+//         likeIconEl.classList.remove("like");
+//     }, 1200);
+
+// });
+
+
+
+
+
+// createSectionEl.innerHTML = `<section>
+//                                  <div class="profile">
+//                                      <img src=${posts[0].avatar} class="profile-img" >
+//                                      <div class="profile-info">
+//                                          <p class="profile-name bold-text" id="user-name"> Vincent van Gogh</p>
+//                                          <p class="profile-location" id="user-location"> Zudert, Netherlands</p>
+//                                      </div>
+//                                  </div>
+//                                  <div class="img-container" id="img-post" >
+//                                          <img src="images/post-vangogh.jpg"> 
+//                                  </div>
+//                                  <div class="comment-section">
+//                                      <div class="icon-section">
+//                                          <img class="comment-icons" src ="images/icon-heart.png">
+//                                          <img class="comment-icons" src ="images/icon-comment.png">
+//                                          <img class="comment-icons" src ="images/icon-dm.png">
+//                                      </div>
+//                                      <p class="like-section bold-text">21 likes</p>
+//                                      <p class="comment"><span class="bold-text" id="user-name">vincey123</span>just took a few mushrooms lol</p>
+//                                  </div>
+//                              </section>`
+
+
+
+// // Create profile 
+// const profileSectionEl = document.createElement("div");
+// profileSectionEl.classList.add("profile");
+
+// // Create img element
+// const imgProfileEl = document.createElement("img");
+// imgProfileEl.classList.add("profile-img");
+// imgProfileEl.setAttribute("src", posts[0].avatar);
+// imgProfileEl.setAttribute("alt", `Portrait of ${posts[0].avatar}`);
+
+// // Create div for profile info
+// const profileInfoEl = document.createElement("div");
+// profileSectionEl.classList.add("profile-info");
+
+// // Create porfile name and location element
+// const profileNameEl = document.createElement("p");
+// profileNameEl.setAttribute("id", "user-name");
+// profileNameEl.classList.add("profile-name");
+// profileNameEl.classList.add("bold-text");
+// profileNameEl.textContent = `${posts[0].name}`;
+
+// const profileLocationEl = document.createElement("p");
+// profileLocationEl.setAttribute("id", "user-location");
+// profileLocationEl.classList.add("profile-location");
+// profileLocationEl.textContent = `${posts[0].location}`;
+
+// // add profileNameEl & profileLocationEl to profileInfoEl, then add imgProfileEl & profileInfoEl to 
+// // profileSectionEl then add that to that to create section.
+// profileInfoEl.append(profileNameEl, profileLocationEl);
+// profileSectionEl.append(imgProfileEl, profileInfoEl);
+// createSectionEl.append(profileSectionEl);
+
+// // create div with class for img post and create img tag
+// const imgContainerEl = document.createElement("div")
+// imgContainerEl.classList.add("img-container");
+// imgContainerEl.setAttribute("id", "img-post");
+// const imgEl = document.createElement("img");
+// imgEl.setAttribute("src", posts[0].post);
+// imgEl.setAttribute("alt", `Self-portrait of ${posts[0].name}`)
+// imgContainerEl.append(imgEl);
+// // Add imgContainer div to section
+// createSectionEl.append(imgContainerEl);
+
+// // create 2 divs: 1st child div containing the comment icons
+// //                parent div containing 2 p tags w/ a span and child div
+// const cmntSectionEl = document.createElement("div");
+// cmntSectionEl.classList.add("comment-section");
+// const iconSectionEl = document.createElement("div");
+// iconSectionEl.classList.add("icon-section");
+
+// // Create img tags for comment icons
+// // Create img elements
+// const iconHeartEl = document.createElement("img");
+// iconHeartEl.classList.add("comment-icons")
+// iconHeartEl.setAttribute("src", "images/icon-heart.png")
+// iconHeartEl.setAttribute("alt", `Heart icon`);
+
+// const iconCmntEl = document.createElement("img");
+// iconCmntEl.classList.add("comment-icons")
+// iconCmntEl.setAttribute("src", "images/icon-comment.png")
+// iconCmntEl.setAttribute("alt", `Comment icon`);
+
+// const iconDmEl = document.createElement("img");
+// iconDmEl.classList.add("comment-icons")
+// iconDmEl.setAttribute("src", "images/icon-dm.png")
+// iconDmEl.setAttribute("alt", `Direct Message icon`);
+
+// // Add the images to icon section div
+// iconSectionEl.append(iconHeartEl, iconCmntEl, iconDmEl);
+
+// // Create 2 p tags with the 2nd p container span 
+// const likeSectionEl = document.createElement("p");
+// likeSectionEl.classList.add("like-section");
+// likeSectionEl.classList.add("bold-text");
+// likeSectionEl.textContent = `${posts[0].likes} likes`;
+
+// const cmntEl = document.createElement("p"); 
+// cmntEl.classList.add("comment");
+// const userNameEl = document.createElement("span");
+// userNameEl.classList.add("bold-text")
+// userNameEl.setAttribute("id", "user-name")
+// userNameEl.textContent = `${posts[0].username}`;
+// cmntEl.append(userNameEl);
+
+// cmntSectionEl.append(iconSectionEl, likeSectionEl, cmntEl);
+// createSectionEl.append(cmntSectionEl);
 
 
 
